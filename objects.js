8cc4ee45dc2ee4dd2a8a5cac719f3dede4d13ba6
John Loberger <johnloberger@gmail.com>
2020-03-24 18:07:23 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {'artistName':'songTitle'}
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist['artistName'] = 'songTitle'
-  return platlist 
+  playlist[artistName] = songTitle
+  return platlist
 }
