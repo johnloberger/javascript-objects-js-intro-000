24ac92aba3dd06f8f8b0769b3de512b615fc7839
John Loberger <johnloberger@gmail.com>
2020-03-24 17:50:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,1 @@
-var playlist = new Object({keys: '0'})
-function updatePlaylist(playlist, artistName, songTitle) {
-playlist[artistName] = songTitle
-return playlist
-}
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist[artistName]
-  return playlist
-}
+var playlist =
